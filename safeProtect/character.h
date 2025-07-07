a39eea3cba5d8ef260d1f27f68a098613cc52d0f
Raul Petran <petranraulfrancesco@gmail.com>
2025-07-07 13:22:21 +0300

    improved effects

--- safeProtect/character.h
+++ safeProtect/character.h
@@ -46,5 +46,6 @@ public:
 extern std::vector <character> chars;
 extern std::vector <projectile> proj;
 extern std::vector <effect> effects;
+extern effect currentEff;
 extern csv_file char_data;
 extern double time_elapsed;
--- safeProtect/effects.h
+++ safeProtect/effects.h
@@ -4,15 +4,17 @@
 class effect
 {
 public:
+	int id;
 	std::string eff;
 	float val;
 	float dur;
 	float activated;
+	bool active;
 
 	//overloading the == function(can be customized)
 	//really cool trick for comparing two objects of the same class(and a lot cleaner)
 	bool operator==(const effect& other)const {
-		return eff == other.eff && val == other.val && dur == other.dur;
+		return eff == other.eff && val == other.val && dur == other.dur && id == other.id;
 	}
 
 	bool operator!=(const effect& other)const {
